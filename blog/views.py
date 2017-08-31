95ebc35cd6fd1f26127e006d0461e079084b9e64
Xavier <xavier982@hotmail.com>
2017-08-31 09:48:11 +0200

    Added 'edit post' and basic security features

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,6 @@ def post_detail(request, pk):
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
-
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -28,4 +27,19 @@ def post_new(request):
 
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
